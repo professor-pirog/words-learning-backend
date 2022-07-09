f64f1701ae791de4231140a10811b86ea031e8fc
Andrey <1992.ivanov.andrey@gmail.com>
2022-07-09 21:36:14 +0400

    add swagger

--- src/main/java/com/pirogsoft/wordslearning/WordsLearningBackendApplication.java
+++ src/main/java/com/pirogsoft/wordslearning/WordsLearningBackendApplication.java
@@ -3,7 +3,6 @@ package com.pirogsoft.wordslearning;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Set;
 
 @SpringBootApplication
 public class WordsLearningBackendApplication {
